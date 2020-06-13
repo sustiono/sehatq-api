66cad8bc6bd38183ec3b0d3c3c9099cb986aec4f
Sustiono <sustiono19@gmail.com>
2020-06-13 22:22:26 +0700

    Doctor & Schedule validation on create appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -25,24 +25,26 @@ class Appointment < ApplicationRecord
     scope: :user_id, message: 'Can only make 1 appointment in 1 schedule'
   }
 
-  before_validation :time_validation, :availbility_schedule_validation
+  before_validation :availbility_doctor_schedule, :time_validation
 
   belongs_to  :patient, class_name: 'User', foreign_key: :user_id
   belongs_to  :doctor_schedule, class_name: 'Schedule', foreign_key: :schedule_id
 
   private
 
-  def time_validation
-    if DateTime.current < doctor_schedule.start_time  || DateTime.current > doctor_schedule.end_time
-      self.errors.add(
-        :time, 'Make an appointment within 30 minutes before the schedule starts'
-      )
+  def availbility_doctor_schedule
+    if doctor_schedule.nil? || doctor_schedule.user_id != self.user_id
+      self.errors.add(:schedule, 'The schedule is not found')
+    elsif User.find_by(id: self.user_id).nil?
+      self.errors.add(:schedule, 'The Doctor is not found')
     end
   end
 
-  def availbility_schedule_validation
-    if doctor_schedule.appointments.size >= 10
-      self.errors.add(:schedule, 'The schedule is full')
+  def time_validation
+    if doctor_schedule.present? && (DateTime.current < doctor_schedule.start_time  || DateTime.current > doctor_schedule.end_time)
+      self.errors.add(
+        :time, 'Make an appointment within 30 minutes before the schedule starts'
+      )
     end
   end
 end
